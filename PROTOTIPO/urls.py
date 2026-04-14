2dae40f81292cd65f9fcea59f5f72c1d3138099c
Fernando Ariel Valdes Fuenzalida <valdes@MacBook-Pro-de-Fernando.local>
2026-04-13 20:09:52 -0400

    add home route

--- PROTOTIPO/urls.py
+++ PROTOTIPO/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
+def home(request):
+    return HttpResponse("🚀 Cotizador PRO funcionando en Render")
+
 urlpatterns = [
+    path("", home),  # 👈 ESTO ES LO IMPORTANTE
     path("admin/", admin.site.urls),
 ]
 
-# servir media en desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
